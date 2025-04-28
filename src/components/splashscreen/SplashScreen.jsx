c975b66ba12927ab14ed554260d101c604680897
senna-js <bloggernolimit@gmail.com>
2025-04-28 14:21:35 +0700

    Update SplashScreen.jsx

--- src/components/splashscreen/SplashScreen.jsx
+++ src/components/splashscreen/SplashScreen.jsx
@@ -174,114 +174,118 @@ function SplashScreen() {
         </div>
       </div>
         <div className="mw-body">
-            <div className="container">
-              <div className="mwb-2col">
-                <div className="mwb-center">
-                  <h1 className="mw-heading">
-                    NontoNime.id - The best site to watch anime online for Free
-                  </h1>
-                  <p>
-                    Do you know that according to Google, the monthly search volume for
-                    anime related topics is up to over 1 Billion times? Anime is famous
-                    worldwide and it is no wonder we've seen a sharp rise in the number of
-                    free anime streaming sites.
-                  </p>
-                  <p>
-                    Just like free online movie streaming sites, anime watching sites are
-                    not created equally, some are better than the rest, so we've decided
-                    to build NontoNime.id to be one of the best free anime streaming site
-                    for all anime fans on the world.
-                  </p>
-                  <h2>1/ What is NontoNime.id?</h2>
-                  <p>
-                    NontoNime.id is a free site to watch anime and you can even download
-                    subbed or dubbed anime in ultra HD quality without any registration or
-                    payment. By having only one ads in all kinds, we are trying to make it
-                    the safest site for free anime.
-                  </p>
-                  <h2>2/ Is NontoNime.id safe?</h2>
-                  <p>
-                    Yes we are, we do have only one Ads to cover the server cost and we
-                    keep scanning the ads 24/7 to make sure all are clean, If you find any
-                    ads that is suspicious, please forward us the info and we will remove
-                    it.
-                  </p>
-                  <h2>
-                    3/ So what make NontoNime.id the best site to watch anime free online?
-                  </h2>
-                  <p>
-                    Before building NontoNime.id, we've checked many other free anime sites,
-                    and learnt from them. We only keep the good things and remove all the
-                    bad things from all the competitors, to put it in our HiAnime website.
-                    Let's see how we're so confident about being the best site for anime
-                    streaming:
-                  </p>
-                  <ul>
-                    <li>
-                      <strong>Safety:</strong> We try our best to not having harmful ads
-                      on HiAnime.
-                    </li>
-                    <li>
-                      <strong>Content library:</strong> Our main focus is anime. You can
-                      find here popular, classic, as well as current titles from all
-                      genres such as action, drama, kids, fantasy, horror, mystery,
-                      police, romance, school, comedy, music, game and many more. All
-                      these titles come with English subtitles or are dubbed in many
-                      languages.
-                    </li>
-                    <li>
-                      <strong>Quality/Resolution:</strong> All titles are in excellent
-                      resolution, the best quality possible. NontoNime.id also has a quality
-                      setting function to make sure our users can enjoy streaming no
-                      matter how fast your Internet speed is. You can stream the anime at
-                      360p if your Internet is being ridiculous, Or if it is good, you can
-                      go with 720p or even 1080p anime.
-                    </li>
-                    <li>
-                      <strong>Streaming experience:</strong> Compared to other anime
-                      streaming sites, the loading speed at NontoNime.id is faster.
-                      Downloading is just as easy as streaming, you won't have any problem
-                      saving the videos to watch offline later.
-                    </li>
-                    <li>
-                      <strong>Updates:</strong> We updates new titles as well as fulfill
-                      the requests on a daily basis so be warned, you will never run out
-                      of what to watch on HiAnime.
-                    </li>
-                    <li>
-                      <strong>User interface:</strong> Our UI and UX makes it easy for
-                      anyone, no matter how old you are, how long have you been on the
-                      Internet. Literally, you can figure out how to navigate our site
-                      after a quick look. If you want to watch a specific title, search
-                      for it via the search box. If you want to look for suggestions, you
-                      can use the site's categories or simply scroll down for new
-                      releases.
-                    </li>
-                    <li>
-                      <strong>Device compatibility:</strong> HiAnime works alright on both
-                      your mobile and desktop. However, we'd recommend you use your
-                      desktop for a smoother streaming experience.
-                    </li>
-                    <li>
-                      <strong>Customer care:</strong> We are in active mode 24/7. You can
-                      always contact us for any help, query, or business-related inquiry.
-                      On our previous projects, we were known for our great customer
-                      service as we were quick to fix broken links or upload requested
-                      content.
-                    </li>
-                  </ul>
-                  <p>
-                    So if you're looking for a trustworthy and safe site for your Anime
-                    streaming, let's give NontoNime.id a try. And if you like us, please
-                    help us to spread the words and do not forget to bookmark our site.
-                  </p>
-                  <p>Thank you!</p>
-                  <p>&nbsp;</p>
-                </div>
-                <div className="clearfix" />
+          <div className="container">
+            <div className="mwb-2col">
+              <div className="mwb-center">
+                <h1 className="mw-heading">
+                  NontoNime.id - The best site to watch anime online for Free
+                </h1>
+                <p>
+                  Do you know that according to Google, the monthly search volume for
+                  anime related topics is up to over 1 Billion times? Anime is famous
+                  worldwide and it is no wonder we've seen a sharp rise in the number of
+                  free anime streaming sites.
+                </p>
+                <p>
+                  Just like free online movie streaming sites, anime watching sites are
+                  not created equally, some are better than the rest, so we've decided
+                  to build NontoNime.id to be one of the best free anime streaming site
+                  for all anime fans on the world.
+                </p>
+                <h2 style={{ fontSize: "1.5rem", margin: "1rem" }}>
+                  1/ What is NontoNime.id?
+                </h2>
+                <p>
+                  NontoNime.id is a free site to watch anime and you can even download
+                  subbed or dubbed anime in ultra HD quality without any registration or
+                  payment. By having only one ads in all kinds, we are trying to make it
+                  the safest site for free anime.
+                </p>
+                <h2 style={{ fontSize: "1.5rem", margin: "1rem" }}>
+                  2/ Is NontoNime.id safe?
+                </h2>
+                <p>
+                  Yes we are, we do have only one Ads to cover the server cost and we
+                  keep scanning the ads 24/7 to make sure all are clean, If you find any
+                  ads that is suspicious, please forward us the info and we will remove
+                  it.
+                </p>
+                <h2 style={{ fontSize: "1.5rem", margin: "1rem" }}>
+                  3/ So what make NontoNime.id the best site to watch anime free online?
+                </h2>
+                <p>
+                  Before building NontoNime.id, we've checked many other free anime
+                  sites, and learnt from them. We only keep the good things and remove
+                  all the bad things from all the competitors, to put it in our HiAnime
+                  website. Let's see how we're so confident about being the best site
+                  for anime streaming:
+                </p>
+                <ul style={{ margin: "1rem", boxSizing: "border-box" }}>
+                  <li>
+                    <strong>Safety:</strong> We try our best to not having harmful ads
+                    on HiAnime.
+                  </li>
+                  <li>
+                    <strong>Content library:</strong> Our main focus is anime. You can
+                    find here popular, classic, as well as current titles from all
+                    genres such as action, drama, kids, fantasy, horror, mystery,
+                    police, romance, school, comedy, music, game and many more. All
+                    these titles come with English subtitles or are dubbed in many
+                    languages.
+                  </li>
+                  <li>
+                    <strong>Quality/Resolution:</strong> All titles are in excellent
+                    resolution, the best quality possible. NontoNime.id also has a
+                    quality setting function to make sure our users can enjoy streaming
+                    no matter how fast your Internet speed is. You can stream the anime
+                    at 360p if your Internet is being ridiculous, Or if it is good, you
+                    can go with 720p or even 1080p anime.
+                  </li>
+                  <li>
+                    <strong>Streaming experience:</strong> Compared to other anime
+                    streaming sites, the loading speed at NontoNime.id is faster.
+                    Downloading is just as easy as streaming, you won't have any problem
+                    saving the videos to watch offline later.
+                  </li>
+                  <li>
+                    <strong>Updates:</strong> We updates new titles as well as fulfill
+                    the requests on a daily basis so be warned, you will never run out
+                    of what to watch on HiAnime.
+                  </li>
+                  <li>
+                    <strong>User interface:</strong> Our UI and UX makes it easy for
+                    anyone, no matter how old you are, how long have you been on the
+                    Internet. Literally, you can figure out how to navigate our site
+                    after a quick look. If you want to watch a specific title, search
+                    for it via the search box. If you want to look for suggestions, you
+                    can use the site's categories or simply scroll down for new
+                    releases.
+                  </li>
+                  <li>
+                    <strong>Device compatibility:</strong> HiAnime works alright on both
+                    your mobile and desktop. However, we'd recommend you use your
+                    desktop for a smoother streaming experience.
+                  </li>
+                  <li>
+                    <strong>Customer care:</strong> We are in active mode 24/7. You can
+                    always contact us for any help, query, or business-related inquiry.
+                    On our previous projects, we were known for our great customer
+                    service as we were quick to fix broken links or upload requested
+                    content.
+                  </li>
+                </ul>
+                <p>
+                  So if you're looking for a trustworthy and safe site for your Anime
+                  streaming, let's give NontoNime.id a try. And if you like us, please
+                  help us to spread the words and do not forget to bookmark our site.
+                </p>
+                <p>Thank you!</p>
+                <p>&nbsp;</p>
               </div>
+              <div className="clearfix" />
             </div>
           </div>
+        </div>
       <div className="mt-10 text-[14px] text-center pb-4">
         © {logoTitle} All rights reserved.
       </div>
