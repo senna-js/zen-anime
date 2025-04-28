2a69dd2d50ce87b52aefcec2a0170d4f9e342d76
senna-js <bloggernolimit@gmail.com>
2025-04-28 12:33:15 +0700

    Update SplashScreen.jsx

--- src/components/splashscreen/SplashScreen.jsx
+++ src/components/splashscreen/SplashScreen.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useCallback } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./SplashScreen.css";
-import "./Grid.scss";
 import logoTitle from "@/src/config/logoTitle";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
